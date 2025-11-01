70a2566b5f586811c8a9b23bdc7b3c39e504ed52
seguraabc <122451838+seguraabc@users.noreply.github.com>
2025-10-31 23:23:10 -0300

    feat: Use environment variables for Google config
    Replaces hardcoded Google API Key and Client ID with environment variables for better security and flexibility.

--- config.ts
+++ config.ts
@@ -4,10 +4,7 @@
 // Sigue los pasos que aparecen en la pantalla de configuración de la aplicación
 // para obtener tu API Key y Client ID, y luego pégalos aquí.
 
-export const GOOGLE_CONFIG = {
-  // 1. Reemplaza "TU_API_KEY" con la API Key que generaste en la Google Cloud Console.
-  API_KEY: 'AIzaSyAjTKl_pqZ5HuZMPAnh90e2txuUW3sN0oc',
-
-  // 2. Reemplaza "TU_CLIENT_ID.apps.googleusercontent.com" con el Client ID que generaste.
-  CLIENT_ID: '191525381309-c8vaer2ngqv6u01pfqtfgisf8oogpo90.apps.googleusercontent.com',
+export const CONFIG = {
+  API_KEY: import.meta.env.VITE_GOOGLE_API_KEY,
+  CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 };
